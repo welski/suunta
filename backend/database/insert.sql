2ae5f12f4a70445a7bd61751e55d51919b8c091c
welski <welc@welc.net>
2017-10-30 09:17:48 +0200

    Tietokantatiedostoja viilattu, pari mock-tiedostoa poistettu

---
+++ backend/database/insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO kysely (
+nimi,
+kuvaus
+) VALUES (
+'Kurssipalaute kurssilta SWD4TN022-5',
+'Anna kurssipalautetta opettajalle kurssilta SWD4TN022-5 vastaamalla annettuihin kysymyksiin.');
