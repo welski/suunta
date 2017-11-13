b68b6df1fe25f79eb5580535a14d19270aebfbb5
Marion Karlsson <marion.karlsson@gmail.com>
2017-11-13 09:05:33 +0200

    kysymys- ja vaihtoehtotaulujen lisäys, sekä insert testimateriaalia

--- backend/database/insert.sql
+++ backend/database/insert.sql
@@ -4,3 +4,46 @@ kuvaus
 ) VALUES (
 'Kurssipalaute kurssilta SWD4TN022-5',
 'Anna kurssipalautetta opettajalle kurssilta SWD4TN022-5 vastaamalla annettuihin kysymyksiin.');
+
+INSERT INTO kysymys (
+teksti,
+monivalinta,
+kysely_id
+) VALUES (
+'Mitkä olivat 3 tärkeintä asiaa jotka opit ohjelmistoprojektin kurssilta?',
+ false,
+ 1
+);
+
+INSERT INTO kysymys (
+teksti,
+monivalinta,
+kysely_id
+) VALUES (
+'Mistä kuulit kurssista? Valitse alla olevista vaihtoehdoista',
+ true,
+ 1
+);
+
+INSERT INTO vaihtoehto (
+teksti,
+kysymys_id
+) VALUES (
+'MyNetistä',
+ 1);
+
+INSERT INTO vaihtoehto (
+teksti,
+kysymys_id
+) VALUES (
+'Kaverilta',
+ 1);
+ 
+ INSERT INTO vaihtoehto (
+teksti,
+kysymys_id
+) VALUES (
+'Opinto-opas',
+ 1);
+
+
