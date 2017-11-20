f59e0a1f21b9649f9be26f32eab7079155cd61a1
Marion Karlsson <marion.karlsson@gmail.com>
2017-11-20 10:23:05 +0200

    insert ja create table dokumenttien päivitys, sekä Vastaus bean-luokan luonti

---
+++ backend/src/main/java/backend/bean/Vastaus.java
@@ -0,0 +1,32 @@
+package backend.bean;
+
+public class Vastaus {
+	
+	private int id;
+	private String teksti;
+	private int kysymysId;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTeksti() {
+		return teksti;
+	}
+	public void setTeksti(String teksti) {
+		this.teksti = teksti;
+	}
+	public int getKysymysId() {
+		return kysymysId;
+	}
+	public void setKysymysId(int kysymysId) {
+		this.kysymysId = kysymysId;
+	}
+	@Override
+	public String toString() {
+		return "Vastaus [id=" + id + ", teksti=" + teksti + ", kysymysId=" + kysymysId + "]";
+	}
+	
+}
